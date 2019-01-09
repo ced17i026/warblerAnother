270c2ba545934cb9da0a778b83ceb2912c295e54
ced17i026 <ced17i026@iiitdm.ac.in>
2019-01-10 01:05:41 +0530

    Add login and signup form

---
+++ client/src/components/AuthForm.js
@@ -0,0 +1,89 @@
+import React,{Component} from 'react';
+import './AuthForm.css';
+
+export default class AuthForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email:'',
+            username:'',
+            password:'',
+            profileImg:""
+        }
+    }
+    handleChange = e=>{
+        this.setState({
+            [e.target.name]:e.target.value,
+        })
+    }
+    handleSubmit = e=>{
+        e.preventDefault();
+        let authType = this.props.signup ? "signup":"signin";
+        this.props.onAuth(authType,this.state).then(()=>{
+            console.log("Logged In");
+            this.redirect();
+        })
+    }
+    render(){
+        const {email,username,profileImg} = this.state;
+        const {heading} = this.props;
+        return(
+            <div className="Form-Body">
+                <h1>{heading}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id='email'
+                        size={45}
+                        onChange={this.handleChange}
+                        value={email}
+                        name="email"
+                    />
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id='password'
+                        size={45}
+                        onChange={this.handleChange}
+                        name="password"
+                    />
+                    {
+                        this.props.signup&&(
+                            <div>
+                                <label htmlFor="username">Username:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id='username'
+                                    size={45}
+                                    onChange={this.handleChange}
+                                    value={username}
+                                    name="username"
+                                />
+                                <label htmlFor="profileImg">ProfileImg:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id='profileImg'
+                                    size={45}
+                                    onChange={this.handleChange}
+                                    value={profileImg}
+                                    name="profileImg"
+                                />
+                            </div>
+                        )
+                    }
+                    <button 
+                    type="submit"
+                    size={45}
+                    className="btn btn-primary">
+                    {heading}
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -2,12 +2,15 @@ import React from "react";
 import {Route,withRouter,Switch} from 'react-router-dom';
 import {connect} from "react-redux";
 import HomePage from "../components/HomePage";
+import AuthForm from "../components/AuthForm";
 
 const Main = (props)=>{
     return(
         <div>
             <Switch>
                 <Route exact path="/" render={props=><HomePage {...props}/>}/>
+                <Route exact path="/signin" render={props=><AuthForm {...props} heading="Log In"/>}/>
+                <Route exact path="/signup" render={props=><AuthForm {...props} heading="Sign Up" signup="signup"/>}/>
             </Switch>
         </div>
     );
